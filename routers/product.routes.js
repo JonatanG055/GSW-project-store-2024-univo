dcb04cdb95d5d3a02020873902f829b369747b5c
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-10-07 14:21:52 -0600

    subiendo endpoint update

--- routers/product.routes.js
+++ routers/product.routes.js
@@ -1,8 +1,12 @@
 import createNewProduct from "../controllers/product.controller.js";
 import express from "express";
+import updateProduct from "../controllers/update.product.controller.js";
+
 
 const Routes = express.Router()
 
 Routes.post("/create-product", createNewProduct)
+
+Routes.put("/update-product/:id", updateProduct)
 
 export default Routes
